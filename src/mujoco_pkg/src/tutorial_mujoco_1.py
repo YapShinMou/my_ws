b81c05ad1312346b011924c3018775768e852539
葉昕謀 <arthurxiyap@gmail.com>
2025-11-25 14:43:05 +0800

    Delete src/mujoco_pkg/src/tutorial_mujoco_1.py

--- src/mujoco_pkg/src/tutorial_mujoco_1.py
+++
@@ -1,9 +0,0 @@
-# simple check model
-import mujoco
-import mujoco.viewer as viewer
-
-# Make model and data
-model = mujoco.MjModel.from_xml_path('../models/cart_pole.xml')
-data = mujoco.MjData(model)
-
-viewer.launch(model, data)
