90e6de2b5d38240acf91c84ea22e98079611cbfb
葉昕謀 <arthurxiyap@gmail.com>
2026-02-01 07:17:12 +0800

    新增儲存匯入buffer

--- src/mujoco_pkg/src/tutor_NN_Batch.py
+++ src/mujoco_pkg/src/tutor_NN_Batch.py
@@ -1,4 +1,4 @@
-# 2025/12/24 範例
+# 2026/1/31 範例
 import numpy as np
 import collections
 import random
@@ -7,11 +7,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-INPUT_DIM = 4
-OUTPUT_DIM = 2
-MEMORY_SIZE = 100
-BATCH_SIZE = 64
-LR = 0.0001
 
 class NeuralNetwork(nn.Module):
    def __init__(self, input_dim, hidden_dim, output_dim):
@@ -29,7 +24,9 @@ class NeuralNetwork(nn.Module):
 
 
 class ReplayBuffer:
-   def __init__(self, input_dim, target_dim, memory_size, batch_size,):
+   def __init__(self, input_dim, target_dim, memory_size, batch_size):
+      self.input_dim = input_dim
+      self.target_dim = target_dim
       self.input_buffer = np.zeros((memory_size, input_dim), dtype=np.float32)
       self.target_buffer = np.zeros((memory_size, target_dim), dtype=np.float32)
       self.memory_size = memory_size
@@ -45,10 +42,39 @@ class ReplayBuffer:
          self.size = self.size + 1
 
    def sample(self):
-      indices = np.random.randint(0, self.memory_size, size=self.batch_size)
+      if self.size < self.batch_size:
+         return None, None
+      indices = np.random.randint(0, self.size, size=self.batch_size)
       input_batch = self.input_buffer[indices]
       target_batch = self.target_buffer[indices]
       return input_batch, target_batch
+
+   def save_buffer(self, file_name):
+      input_buffer_ = np.resize(self.input_buffer, (self.size, self.input_dim))
+      target_buffer_ = np.resize(self.target_buffer, (self.size, self.target_dim))
+      np.savez(file_name, a=input_buffer_, b=target_buffer_)
+      print(f"save buffer: {file_name}")
+
+   def load_buffer(self, file_name):
+      buffer = np.load(file_name)
+      self.input_buffer = np.resize(self.input_buffer, (self.size, self.input_dim))
+      self.target_buffer = np.resize(self.target_buffer, (self.size, self.target_dim))
+      self.input_buffer = np.concatenate((buffer['a'], self.input_buffer))
+      self.target_buffer = np.concatenate((buffer['b'], self.target_buffer))
+      if self.input_buffer.shape[0] < self.memory_size:
+         self.pointer = self.input_buffer.shape[0]
+         self.input_buffer = np.concatenate((self.input_buffer, np.zeros((self.memory_size - self.input_buffer.shape[0], self.input_dim), dtype=np.float32)))
+         self.target_buffer = np.concatenate((self.target_buffer, np.zeros((self.memory_size - self.target_buffer.shape[0], self.target_dim), dtype=np.float32)))
+         self.size = self.input_buffer.shape[0]
+         print(f"load buffer")
+      else:
+         self.input_buffer = self.input_buffer[self.input_buffer.shape[0] - self.memory_size:self.input_buffer.shape[0], :]
+         self.target_buffer = self.target_buffer[self.target_buffer.shape[0] - self.memory_size:self.target_buffer.shape[0], :]
+         self.pointer = 0
+         self.size = self.memory_size
+         print(f"load buffer, buffer overflow")
+
+      # 減0 載buffer, 合併buffer, 訓練, memory_size, [self.input_buffer.shape[0] - self.memory_size:self.input_buffer.shape[0], :]
 
 
 class deep_learning:
@@ -56,7 +82,7 @@ class deep_learning:
       self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
       print(f"Using {self.device} device")
       self.net_1 = NeuralNetwork(input_dim, hidden_dim, output_dim).to(self.device)
-      self.optimizer_1 = torch.optim.Adam(self.net_1.parameters())
+      self.optimizer_1 = torch.optim.Adam(self.net_1.parameters(), lr=0.001)
       self.loss_fn = nn.MSELoss()
 
    def predict(self, input):
@@ -67,6 +93,9 @@ class deep_learning:
 
    def train(self, buffer):
       input_batch, target_batch = buffer.sample()
+      if input_batch is None:
+         print(f"buffer 不夠大，跳過訓練")
+         return
       input_torch = torch.tensor(input_batch, dtype=torch.float32).to(self.device)
       target_torch = torch.tensor(target_batch, dtype=torch.float32).to(self.device)
 
@@ -77,21 +106,28 @@ class deep_learning:
       loss.backward()
       self.optimizer_1.step()
 
-   def save_model(self):
-      torch.save(self.net_1, "net_1.pt")
-      print(f"save model")
+   def save_model(self, file_name):
+      torch.save(self.net_1, file_name)
+      print(f"save model: {file_name}")
 
-   def load_model(self):
-      self.net_1 = torch.load("net_1.pt", weights_only=False)
+   def load_model(self, file_name):
+      self.net_1 = torch.load(file_name, weights_only=False)
       self.net_1.to(self.device)
-      self.optimizer_1 = torch.optim.Adam(self.net_1.parameters())
+      self.optimizer_1 = torch.optim.Adam(self.net_1.parameters(), lr=0.001)
       self.net_1.train()
-      print(f"load model")
+      print(f"load model: {file_name}")
 
 
 def main():
+   INPUT_DIM = 4
+   OUTPUT_DIM = 2
+   MEMORY_SIZE = 50
+   BATCH_SIZE = 32
+
    DL = deep_learning(INPUT_DIM, 32, OUTPUT_DIM)
+   # DL.load_model("net_1.pt")
    memory = ReplayBuffer(INPUT_DIM, OUTPUT_DIM, MEMORY_SIZE, BATCH_SIZE)
+   # memory.load_buffer('buffer.npz')
 
    a_input = np.array([1.0, 2.0, 3.0, 4.0], dtype=np.float32)
    a_target = np.array([3.0, 7.0], dtype=np.float32)
@@ -106,9 +142,7 @@ def main():
    output_c = DL.predict(c_input)
    print(f"untrain output: {output_a} {output_b} {output_c}")
 
-   DL.load_model()
-
-   for i in range(150):
+   for i in range(40):
       intput_1 = np.random.random()*20 - 10
       intput_2 = np.random.random()*20 - 10
       intput_3 = np.random.random()*20 - 10
@@ -125,7 +159,8 @@ def main():
    output_c = DL.predict(c_input)
    print(f"trained output: {output_a} {output_b} {output_c}")
 
-   DL.save_model()
+   # DL.save_model("net_1.pt")
+   # memory.save_buffer('buffer.npz')
 
 
 start_time = time.time()
